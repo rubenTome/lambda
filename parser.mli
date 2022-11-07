b1de2838b2f0543595d409ae729a3e7e3839685b
rubenTome <72791927+rubenTome@users.noreply.github.com>
2022-11-07 20:10:36 +0100

    Add files via upload
    lambda-3 con letrec añadido

---
+++ parser.mli
@@ -0,0 +1,27 @@
+type token =
+  | LAMBDA
+  | TRUE
+  | FALSE
+  | IF
+  | THEN
+  | ELSE
+  | SUCC
+  | PRED
+  | ISZERO
+  | LET
+  | LETREC
+  | IN
+  | BOOL
+  | NAT
+  | LPAREN
+  | RPAREN
+  | DOT
+  | EQ
+  | COLON
+  | ARROW
+  | EOF
+  | INTV of (int)
+  | STRINGV of (string)
+
+val s :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Lambda.term
