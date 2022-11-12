d54269bf3ecc87307c1e7303563477460ca90137
rubenTome <rubentome27@gmail.com>
2022-11-12 13:21:13 +0100

    primera version de leer varias lineas hasta ;;

--- main.ml
+++ main.ml
@@ -6,13 +6,25 @@ open Lambda;;
 open Parser;;
 open Lexer;;
 
+open String;;
+
+let rec read_multiline str = 
+  try 
+    flush stdout;
+    sub str 0 (rindex_from str ((rindex str ';') - 1) ';' - 1)
+  with 
+    Not_found ->     
+      let str2 = input_line stdin 
+      in read_multiline (str^str2)
+;;
+
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
   let rec loop ctx =
     print_string ">> ";
     flush stdout;
     try
-      let tm = s token (from_string (read_line ())) in
+      let tm = s token (from_string (read_multiline "")) in
       let tyTm = typeof ctx tm in
       print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm);
       loop ctx
